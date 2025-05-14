384c91659cce833d063beabcbbff85e4cfae63bf
Jason Fisher <jason.fisher15@gmail.com>
2025-05-13 20:17:09 -0400

    Fixes for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -35,6 +35,10 @@ DEBUG = env("DEBUG", default="False") == "True"
 
 ALLOWED_HOSTS = env("ALLOWD_HOSTS", default=["localhost"])
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://awaken.fit",
+]
+
 
 # Application definition
 
